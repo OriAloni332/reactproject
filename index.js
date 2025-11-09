f3a4cf51435164c5944935a6ec07ace25c778a57
Idan Zairi <idanzairi302@gmail.com>
2025-11-09 14:46:30 +0200

    Added .env file to the project using it in index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-res.send('Hello World!')
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+
+  res.send("Hello World!");
+
 });
+
 app.listen(port, () => {
+
   console.log(`Example app listening on port ${port}!`);
+
 });
